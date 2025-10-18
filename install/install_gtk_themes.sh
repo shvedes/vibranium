f7e4b0c1526d1be1b16101893d56c1f25d8125c2
shvedes <shved.personal@gmail.com>
2025-10-19 00:18:56 +0200

    update

--- install/install_gtk_themes.sh
+++ install/install_gtk_themes.sh
@@ -237,5 +237,5 @@ for repo in "${REPOS[@]}"; do
 done
 
 ln -s "$HOME/.themes" "$HOME/.local/share/themes"
-printf "\r\033%s[VIBRANIUM]%s GTK themes installed%s\n" "${YELLOW}" "${GREEN}" "${RESET}"
+printf "\r\033%s[VIBRANIUM]%s GTK themes installed\n" "${YELLOW}" "${RESET}"
 sleep 0.5
