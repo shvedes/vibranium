55b772927cc0afe87b22f9cf7dd747d246a7ccb1
shvedes <shved.personal@gmail.com>
2025-10-18 10:38:48 +0200

    update

--- install/install_papirus_icons.sh
+++ install/install_papirus_icons.sh
@@ -2,24 +2,28 @@
 
 set -euo pipefail
 
+YELLOW=$'\e[0;33m'
+GREEN=$'\e[0;32m'
+RESET=$'\e[0m'
+
 # Due to the active use of the papirus-folders script, 
 # we need to keep the Papirus icon pack accessible for writing by regular users in order to avoid constant use of sudo.
 
 CWD="$(pwd)"
 
-echo "Installing Papirus icon theme"
+echo -e "${YELLOW}[VIBRANIUM]${RESET} Installing icon theme"
 cd ~/.cache
 git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
 cd papirus-icon-theme
 mkdir -p "$HOME/.local/share/icons"
 cp -r Papirus* "$HOME"/.local/share/icons
-echo "Papirus icon theme installed"
+echo -e "${GREEN}[VIBRANIUM]${RESET} Icon theme installed"
 
-echo "Downloading papirus-folders"
+echo -e "${YELLOW}[VIBRANIUM]${RESET} Installing icon theme patcher"
 mkdir -p "$HOME/.local/bin"
 curl -s https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/refs/heads/master/install.sh -o "$HOME/.local/bin/papirus-folders"
 chmod +x "$HOME/.local/bin/papirus-folders"
-echo "papirus-folders installed"
+echo -e "${GREEN}[VIBRANIUM]${RESET} Icon theme patcher installed"
 
-rm -rf ~/.cache/papirus-icon-theme
+rm -rf "$HOME"/.cache/papirus-icon-theme
 cd "$CWD"
