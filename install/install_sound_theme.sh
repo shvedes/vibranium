3f0b76bdefa9c012cae664de45dad8893422207a
shvedes <shved.personal@gmail.com>
2025-11-24 13:11:55 +0100

    cleanup(core-volume): download yaru sound manually to speed up the installation and get rid of a dependency

---
+++ install/install_sound_theme.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+DIR="$HOME/.local/share/sounds/Vibranium/stereo"
+LINK="https://github.com/ubuntu/yaru/raw/refs/heads/master/sounds/src/stereo/audio-volume-change.oga"
+FILE="${DIR}/audio-volume-change.oga"
+
+mkdir -p "$DIR"
+wget -q -4 "$LINK" -O "$FILE"
