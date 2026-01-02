86a14ea268eae91f9ad8692c84f6b8fe1f8679d1
shvedes <shved.personal@gmail.com>
2026-01-02 16:08:57 +0100

    chore(installer): adapt for new hyprpaper syntax

--- install/generate_hyprpaper_config.sh
+++ install/generate_hyprpaper_config.sh
@@ -2,6 +2,6 @@
 
 cat << EOF > "$HOME/.config/hypr/hyprpaper.conf"
 # Auto-generated file! All changes will be overwritten.
-preload = $HOME/.local/share/vibranium/themes/nightfox-nightfox/backgrounds/01-nightfox-bg.jpg
-wallpaper = , $HOME/.local/share/vibranium/themes/nightfox-nightfox/backgrounds/01-nightfox-bg.jpg
+wallpaper:monitor =
+wallpaper:path = $HOME/.local/share/vibranium/themes/nightfox-nightfox/backgrounds/01-nightfox-bg.jpg
 EOF
