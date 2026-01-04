bd222ce23fd74e30b7227b63eeade5d171980eed
shvedes <shved.personal@gmail.com>
2026-01-04 17:08:36 +0100

    feat(project): add desktop entries for bluetui & wiremix

---
+++ migrations/1767539357.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -sf "$VIBRANIUM/applications/wiremix.desktop" "$HOME/.local/share/applications/wiremix.desktop"
+ln -sf "$VIBRANIUM/applications/bluetui.desktop" "$HOME/.local/share/applications/bluetui.desktop"
