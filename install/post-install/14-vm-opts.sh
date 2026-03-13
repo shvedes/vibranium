c3c1d52e51a74d4c69df590238bb1d3ef13adba7
shvedes <shved.personal@gmail.com>
2026-03-13 20:26:16 +0100

    chore: small changes here and there

--- install/post-install/14-vm-opts.sh
+++ install/post-install/14-vm-opts.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 waybar_cfg="$XDG_CONFIG_HOME/waybar/config.jsonc"
+hypr_cfg="$HOME/.config/hypr/hyprland.conf.d/look-and-feel.conf"
 
 if [[ "$CHASSIS_TYPE" == vm ]]; then
     # Disable animations
-    vb-cmd-edit-wm-config "animations:enabled:false" \
-        "$XDG_CONFIG_HOME/hypr/hyprland.conf.d/look-and-feel.conf"
+    vb-cmd-edit-wm-config "animations:enabled:false" "$hypr_cfg"
 
     # Bluetooth & Night Light
     rm ~/.local/share/applications/bluetui.desktop
     sed -i '/\"bluetooth\"/s/\"/\/\/ /' "$waybar_cfg"
     sed -i '/\"custom\/nightshift\"/s/\"/\/\/ /' "$waybar_cfg"
-    systemctl -q --user restart waybar
 fi
+
