9f8bcbce2bd57583faf77f63afd9b171f45614b6
shvedes <shved.personal@gmail.com>
2026-03-13 20:08:31 +0100

    chore: move wev to optional packages

--- install/packaging/05-additional.sh
+++ install/packaging/05-additional.sh
@@ -2,8 +2,8 @@
 
 packages=()
 
-if term::ask_yes_no Y "Would you like to install optional packages?"; then
-    packages+=(pass)
+if term::ask_yes_no Y "Would you like to install optional but not mandatory packages?"; then
+    packages+=(pass wev nwg-look)
 fi
 
 if (( ${#packages[@]} > 0 )); then
