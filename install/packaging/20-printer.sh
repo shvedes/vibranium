c20f011a639fe5e8ace5bd0307f9e122017daef5
shvedes <shved.personal@gmail.com>
2026-03-15 12:39:54 +0100

    feat: add more CUPS-related packages

--- install/packaging/20-printer.sh
+++ install/packaging/20-printer.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# The idea is taken from Omarchy.
-# All cretids to the Omarchy contributors, not me.
-# This is just an adoptation.
-
 if [[ "$CHASSIS_TYPE" == vm ]]; then
   exit 0
 fi
@@ -12,7 +8,26 @@ if ! term::ask_yes_no N "Would you like to setup CUPS printing service?"; then
   exit 0
 fi
 
-InstallPackages cups cups-browsed
+packages(
+  cups
+  cups-pdf
+  cups-browsed
+  cups-filters
+
+  # https://wiki.archlinux.org/title/CUPS#Printer_drivers
+  gutenprint
+  foomatic-db
+  foomatic-db-ppds
+  foomatic-db-nonfree
+  foomatic-db-nonfree-ppds
+  foomatic-db-gutenprint-ppds
+)
+
+InstallPackages "${packages[@]}"
+
+# The code below is copy-pasted from Omarchy.
+# All cretids to the Omarchy contributors, not me.
+# This is just an adoptation.
 
 # Disable multicast dns in resolved. Avahi will provide this for better network printer discovery
 sudo mkdir -p /etc/systemd/resolved.conf.d
@@ -26,5 +41,5 @@ if ! grep -q '^CreateRemotePrinters Yes' /etc/cups/cups-browsed.conf; then
   echo 'CreateRemotePrinters Yes' | sudo tee -a /etc/cups/cups-browsed.conf > /dev/null
 fi
 
-sudo systemctl -q enable cups cups-browsed avahi-daemon
+sudo systemctl -q enable cups.socket cups-browsed avahi-daemon
 UpdateSummary "User choice: enabled CUPS printing service with network printer discovery"
