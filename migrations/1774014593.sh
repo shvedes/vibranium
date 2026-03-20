91af1f48cfd18b0537bd4389a13d90e195c0c91d
shvedes <shved.personal@gmail.com>
2026-03-20 15:23:03 +0100

    update migration for b55b6fc

--- migrations/1774014593.sh
+++ migrations/1774014593.sh
@@ -3,18 +3,21 @@
 vb-pkg-install --embedded --no-verify --no-ask --force-install -- adw-gtk-theme papirus-icon-theme papirus-folders
 
 rm -rf "$XDG_CONFIG_HOME"/qt?ct
+rm -rf "$XDG_DATA_HOME"/icons/Papirus*
+rm -rf "$XDG_CONFIG_HOME"/gtk-?.0/settings.ini
+rm -rf "$XDG_DATA_HOME/.themes" "$HOME/.themes"
+rm -rf "$HOME/.local/bin/papirus-folders"
+
 cp -r "$VIBRANIUM"/config/qt?ct "$XDG_CONFIG_HOME"
 
 if [[ "$(vb-theme-get)" =~ (dawm|latte|light)$ ]]; then
   gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
   gsettings set org.gnome.desktop.interface color-scheme 'default'
   gsettings set org.gnome.desktop.interface icon-theme "Papirus-Light"
-  icon_theme="Papirus-Dark"
 else
   gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
   gsettings set org.gnome.desktop.interface color-scheme 'default'
   gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
-  icon_theme="Papirus-Light"
 fi
 
-sudo papirus-folders --color black --once --theme "$icon_theme" &> /dev/null
+sudo papirus-folders --color black --once &> /dev/null
