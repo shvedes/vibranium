6418fe4f1e1acfff937f82c34ab8ddb46d060957
shvedes <shved.personal@gmail.com>
2026-03-24 10:58:24 +0100

    chore: add migration for qtct template

---
+++ migrations/1774311677.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ~/.config/qt{5,6}ct/colors
+
+for v in 5 6; do
+  ln -sf ~/.config/vibranium/current/theme/qtct.conf ~/.config/qt${v}ct/colors/Vibranium.conf
+  sed -i '/custom_palette/s/=.*/true/' ~/.config/qt${v}ct/qt${v}ct.conf
+
+  sed -i "|custom_palette|s|=.*|/home/$USER/.config/vibranium/current/theme/qtct.conf|" \
+    ~/.config/qt${v}ct/qt${v}ct.conf
+done
