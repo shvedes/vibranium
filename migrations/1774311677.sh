d5b7ad9a8cd4eca937bcd0c632fd7cfd86670a15
shvedes <shved.personal@gmail.com>
2026-03-24 11:00:47 +0100

    fix: fix sed expression

--- migrations/1774311677.sh
+++ migrations/1774311677.sh
@@ -6,6 +6,6 @@ for v in 5 6; do
   ln -sf ~/.config/vibranium/current/theme/qtct.conf ~/.config/qt${v}ct/colors/Vibranium.conf
   sed -i '/custom_palette/s/=.*/true/' ~/.config/qt${v}ct/qt${v}ct.conf
 
-  sed -i "|custom_palette|s|=.*|/home/$USER/.config/vibranium/current/theme/qtct.conf|" \
+  sed -i "s|^custom_palette=.*|custom_palette=/home/$USER/.config/vibranium/current/theme/qtct.conf|" \
     ~/.config/qt${v}ct/qt${v}ct.conf
 done
