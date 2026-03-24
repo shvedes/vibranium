efe21b428fb8629486f2136f08f2fe359dec8acc
shvedes <shved.personal@gmail.com>
2026-03-24 11:35:29 +0100

    update migration

--- migrations/1774311677.sh
+++ migrations/1774311677.sh
@@ -5,6 +5,9 @@ mkdir -p ~/.config/qt{5,6}ct/colors
 for v in 5 6; do
   cfg="$HOME/.config/qt${v}ct/qt${v}ct.conf"
   theme="$HOME/.config/vibranium/current/theme/qtct.conf"
+  theme_out=$HOME/.config/qt${v}ct/colors/Vibranium.conf
+
+  ln -sf "$theme" ~/.config/qt${v}ct/colors/Vibranium.conf
 
   if grep -q '^custom_palette=' "$cfg"; then
     sed -i 's/^custom_palette=.*/custom_palette=true/' "$cfg"
@@ -13,8 +16,8 @@ for v in 5 6; do
   fi
 
   if grep -q '^color_scheme_path=' "$cfg"; then
-    sed -i "s|^color_scheme_path=.*|color_scheme_path=$theme|" "$cfg"
+    sed -i "s|^color_scheme_path=.*|color_scheme_path=$theme_out|" "$cfg"
   else
-    sed -i "/^\[Appearance\]/a color_scheme_path=$theme" "$cfg"
+    sed -i "/^\[Appearance\]/a color_scheme_path=$theme_out" "$cfg"
   fi
 done
