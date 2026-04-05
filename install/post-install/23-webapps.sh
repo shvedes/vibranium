bed00eb6ab0fcbcc9e95b2d5270c9b0e8df7cb0d
shvedes <shved.personal@gmail.com>
2026-04-05 23:53:47 +0200

    feat!: fine-tune PWA installer to fit Vibranium standards & use it in Vibranium installer

---
+++ install/post-install/23-webapps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+_log_info "Installing PWAs..."
+
+vb-webapp-install --category AI --url 'chatgpt.com' --icon chatgpt --name ChatGPT
+vb-webapp-install --category AI --url 'grok.com' --icon grok --name Grok
+vb-webapp-install --category AI --url 'perplexity.ai' --icon perplexity --name Perplexity
+vb-webapp-install --category AI --url 'gemini.google.com' --icon google-gemini --name Gemini
+vb-webapp-install --category AI --url 'claude.ai/chats' --icon claude --name Claude
+vb-webapp-install --category AI --url 'chat.deepseek.com' --icon deepseek --name DeepSeek
+
+vb-webapp-install --category PWA --url 'mail.google.com/mail/u/0/#inbox' --icon gmail --name Gmail
+vb-webapp-install --category PWA --url 'youtube.com' --icon youtube --name YouTube
+vb-webapp-install --category PWA --url 'reddit.com' --icon reddit --name Reddit
+vb-webapp-install --category PWA --url 'github.com' --icon github --name GitHub
+vb-webapp-install --category PWA --url 'x.com' --icon x-twitter --name X
