8653c43bf96012e77376304264ab54a2618527d3
shvedes <shved.personal@gmail.com>
2026-04-06 20:32:38 +0200

    chore: PWA installer / remover QOL improvements

--- install/post-install/23-webapps.sh
+++ install/post-install/23-webapps.sh
@@ -9,8 +9,8 @@ vb-webapp-install --category AI --url 'gemini.google.com' --icon google-gemini -
 vb-webapp-install --category AI --url 'claude.ai/chats' --icon claude --name Claude
 vb-webapp-install --category AI --url 'chat.deepseek.com' --icon deepseek --name DeepSeek
 
-vb-webapp-install --category PWA --url 'mail.google.com/mail/u/0/#inbox' --icon gmail --name Gmail
-vb-webapp-install --category PWA --url 'youtube.com' --icon youtube --name YouTube
-vb-webapp-install --category PWA --url 'reddit.com' --icon reddit --name Reddit
-vb-webapp-install --category PWA --url 'github.com' --icon github --name GitHub
-vb-webapp-install --category PWA --url 'x.com' --icon x-twitter --name X
+vb-webapp-install --category WebApp --url 'mail.google.com/mail/u/0/#inbox' --icon gmail --name Gmail
+vb-webapp-install --category WebApp --url 'youtube.com' --icon youtube --name YouTube
+vb-webapp-install --category WebApp --url 'reddit.com' --icon reddit --name Reddit
+vb-webapp-install --category WebApp --url 'github.com' --icon github --name GitHub
+vb-webapp-install --category WebApp --url 'x.com' --icon x-twitter --name X
