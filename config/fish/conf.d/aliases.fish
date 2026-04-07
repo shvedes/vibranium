d433285402c40e6b965bed976f04f2b741e1c2a1
shvedes <shved.personal@gmail.com>
2026-04-07 22:05:28 +0200

    refactor!: migrate to xdg-terminal-exec & app2unit

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -1,13 +1,13 @@
 if command -q eza
-  alias ls="eza -XM"
-  alias ll="eza -XMhml --no-filesize --smart-group --time-style='+%y-%m-%d %H:%M'"
-  alias la="eza -1lXMhA --no-filesize --smart-group --time-style='+%y-%m-%d %H:%M'"
-  alias tree="eza --tree"
+    alias ls="eza -XM"
+    alias ll="eza -XMhml --no-filesize --smart-group --time-style='+%y-%m-%d %H:%M'"
+    alias la="eza -1lXMhA --no-filesize --smart-group --time-style='+%y-%m-%d %H:%M'"
+    alias tree="eza --tree"
 end
 
 alias imv="imv-dir"
 alias ip="ip --color"
 
 if test -f $VIBRANIUM_STATE/update.available
-  alias update-vibranium="vb-core-term --floating -- vb-update"
+    alias update-vibranium="xdg-terminal-exec --app-id=org.vb.term.float --title=vb-update -- vb-update"
 end
--- install/first-run/30-fix-nvim-gui.sh
+++ install/first-run/30-fix-nvim-gui.sh
@@ -2,5 +2,5 @@
 
 # https://www.reddit.com/r/linuxquestions/comments/t7ze3c/thunar_open_file_in_neovim/
 cp /usr/share/applications/nvim.desktop "$HOME"/.local/share/applications
-sed -e '/Terminal/s/true/false/' -e '/^Exec/s/=/=vb-core-term -- /' \
+sed -e '/Terminal/s/true/false/' -e '/^Exec/s/=/=xdg-terminal-exec -- /' \
   -i "$HOME"/.local/share/applications/nvim.desktop
