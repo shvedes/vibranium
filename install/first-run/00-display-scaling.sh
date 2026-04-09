8104ec9fa44380d8972b1a350a3f16d6c65a759a
shvedes <shved.personal@gmail.com>
2026-04-09 13:53:10 +0200

    feat(vm resolution): explicitly set 1280x720 for a VM installation

--- install/first-run/00-display-scaling.sh
+++ install/first-run/00-display-scaling.sh
@@ -5,38 +5,38 @@
 # Get the active monitor (focused)
 MONITOR_INFO=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true)')
 
-ACTIVE_MONITOR=$(jq -r '.name' <<< "$MONITOR_INFO")
-CURRENT_SCALE=$(jq -r '.scale' <<< "$MONITOR_INFO")
-WIDTH=$(jq -r '.width' <<< "$MONITOR_INFO")
-HEIGHT=$(jq -r '.height' <<< "$MONITOR_INFO")
-REFRESH_RATE=$(jq -r '.refreshRate' <<< "$MONITOR_INFO")
+ACTIVE_MONITOR=$(jq -r '.name' <<<"$MONITOR_INFO")
+CURRENT_SCALE=$(jq -r '.scale' <<<"$MONITOR_INFO")
+WIDTH=$(jq -r '.width' <<<"$MONITOR_INFO")
+HEIGHT=$(jq -r '.height' <<<"$MONITOR_INFO")
+REFRESH_RATE=$(jq -r '.refreshRate' <<<"$MONITOR_INFO")
 
 # Auto scale based on HEIGHT (more reliable than WIDTH for different aspect ratios)
-if (( HEIGHT <= 720 )); then
+if ((HEIGHT <= 720)); then
   NEW_SCALE=1
-elif (( HEIGHT <= 900 )); then
+elif ((HEIGHT <= 900)); then
   NEW_SCALE=1
-elif (( HEIGHT <= 1080 )); then
+elif ((HEIGHT <= 1080)); then
   NEW_SCALE=1
-elif (( HEIGHT <= 1200 )); then
+elif ((HEIGHT <= 1200)); then
   NEW_SCALE=1.1
-elif (( HEIGHT <= 1440 )); then
+elif ((HEIGHT <= 1440)); then
   NEW_SCALE=1.25
-elif (( HEIGHT <= 1600 )); then
+elif ((HEIGHT <= 1600)); then
   NEW_SCALE=1.4
-elif (( HEIGHT <= 1800 )); then
+elif ((HEIGHT <= 1800)); then
   NEW_SCALE=1.5
-elif (( HEIGHT <= 2160 )); then
+elif ((HEIGHT <= 2160)); then
   NEW_SCALE=2
-elif (( HEIGHT <= 2400 )); then
+elif ((HEIGHT <= 2400)); then
   NEW_SCALE=2
-elif (( HEIGHT <= 2880 )); then
+elif ((HEIGHT <= 2880)); then
   NEW_SCALE=2.5
-elif (( HEIGHT <= 3200 )); then
+elif ((HEIGHT <= 3200)); then
   NEW_SCALE=2.5
-elif (( HEIGHT <= 3840 )); then
+elif ((HEIGHT <= 3840)); then
   NEW_SCALE=3
-elif (( HEIGHT <= 4320 )); then
+elif ((HEIGHT <= 4320)); then
   NEW_SCALE=3.5
 else
   NEW_SCALE=4
@@ -49,4 +49,8 @@ hyprctl -q keyword misc:disable_scale_notification false
 notify-send -r $RANDOM -t 7000 "Vibranium" "Display scaling set to ${NEW_SCALE}x"
 
 # Save to config file
-echo "monitor = $ACTIVE_MONITOR, ${WIDTH}x${HEIGHT}@${REFRESH_RATE}, auto, $NEW_SCALE" >> "$XDG_CONFIG_HOME/hypr/hyprland.conf.d/monitors.conf"
+if [[ $CHASSIS_TYPE == vm ]]; then
+  echo "monitor = $ACTIVE_MONITOR, 1280x$720@${REFRESH_RATE}, auto, 1" >>"$XDG_CONFIG_HOME/hypr/hyprland.conf.d/monitors.conf"
+else
+  echo "monitor = $ACTIVE_MONITOR, ${WIDTH}x${HEIGHT}@${REFRESH_RATE}, auto, $NEW_SCALE" >>"$XDG_CONFIG_HOME/hypr/hyprland.conf.d/monitors.conf"
+fi
