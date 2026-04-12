9379020e79e2f567f46db617394ab48a4c0f8c81
shvedes <shved.personal@gmail.com>
2026-04-12 22:04:20 +0200

    feat: create bookmarks for QT file picker as well

--- install/first-run/14-configure-gtk-bookmarks.sh
+++ install/first-run/14-configure-gtk-bookmarks.sh
@@ -5,14 +5,14 @@
 
 file="$XDG_CONFIG_HOME/user-dirs.dirs"
 
-if [[ ! "$file" ]]; then
-  msg="$file does not exists! No default folder & bookmarks were created.\n"
+if [[ ! -f "$file" ]]; then
+  msg="$file does not exist! No default folders & bookmarks were created.\n"
 
   if command -v xdg-user-dir >/dev/null; then
-    msg=+"To fix, run 'systemctl --user enable --now xdg-user-dirs'"
+    msg+="To fix, run 'systemctl --user enable --now xdg-user-dirs'"
   else
     msg+="To fix, install xdg-user-dirs and run\n"
-    msg=+"'systemctl --user start xdg-user-dirs'"
+    msg+="'systemctl --user start xdg-user-dirs'"
   fi
 
   notify-send -r $RANDOM -t 30000 -u critical \
@@ -31,5 +31,14 @@ _parse_user_dirs() {
   done <"$file"
 }
 
+mapfile -t _xdg_dirs < <(_parse_user_dirs)
+
 mkdir -p "$XDG_CONFIG_HOME/gtk-3.0"
-_parse_user_dirs >"$XDG_CONFIG_HOME/gtk-3.0/bookmarks"
+printf '%s\n' "${_xdg_dirs[@]}" >"$XDG_CONFIG_HOME/gtk-3.0/bookmarks"
+
+_qt_shortcuts="file:"
+for _dir in "${_xdg_dirs[@]}"; do
+  _qt_shortcuts+=", $_dir"
+done
+
+sed -i "s|^shortcuts=.*|shortcuts=$_qt_shortcuts|" "$XDG_CONFIG_HOME/QtProject.conf"
