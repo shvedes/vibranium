eec017a429be0bd9bd5888237839991959384c7c
shvedes <shved.personal@gmail.com>
2026-04-14 14:39:14 +0200

    chore: refresh the Lumon theme, add Neovim colors

--- default/hooks/startup/04-source-loopback.sh
+++ default/hooks/startup/04-source-loopback.sh
@@ -5,6 +5,8 @@ while ! pidof -q pipewire; do
   sleep 0.1
 done
 
+# The loopback will be disabled by default because we're creating it
+# in runtime without permanent configurations, so we need to restore it this way.
 helpers::check VIBRANIUM_VOLUME_SOURCE_LOOPBACK
 if [[ $VIBRANIUM_VOLUME_SOURCE_LOOPBACK == true ]]; then
   vb-cmd-toggle-loopback
