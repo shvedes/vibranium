023b2920cbfc8dcdf3153a6a27d5096a4def3c2e
shvedes <shved.personal@gmail.com>
2026-04-14 19:28:07 +0200

    feat: add TUI installation process

--- install/post-install/23-webapps.sh
+++ install/post-install/23-webapps.sh
@@ -2,15 +2,68 @@
 
 _log_info "Installing PWAs..."
 
-vb-webapp-install --category AI --url 'chatgpt.com' --name ChatGPT --icon chatgpt
-vb-webapp-install --category AI --url 'grok.com' --name Grok --icon grok
-vb-webapp-install --category AI --url 'perplexity.ai' --name Perplexity --icon perplexity
-vb-webapp-install --category AI --url 'gemini.google.com' --name Gemini --icon google-gemini
-vb-webapp-install --category AI --url 'claude.ai/chats' --name Claude --icon claude
-vb-webapp-install --category AI --url 'chat.deepseek.com' --name DeepSeek --icon deepseek
+vb-webapp-install \
+  --category AI \
+  --url 'chatgpt.com' \
+  --name ChatGPT \
+  --icon chatgpt
 
-vb-webapp-install --category WebApp --url 'mail.google.com/mail/u/0/#inbox' --icon gmail --name Gmail
-vb-webapp-install --category WebApp --url 'youtube.com' --icon youtube --name YouTube
-vb-webapp-install --category WebApp --url 'reddit.com' --icon reddit --name Reddit
-vb-webapp-install --category WebApp --url 'github.com' --icon github --name GitHub
-vb-webapp-install --category WebApp --url 'x.com' --icon twitter-x --name X
+vb-webapp-install \
+  --category AI \
+  --url 'grok.com' \
+  --name Grok \
+  --icon grok
+
+vb-webapp-install \
+  --category AI \
+  --url 'perplexity.ai' \
+  --name Perplexity \
+  --icon perplexity
+
+vb-webapp-install \
+  --category AI \
+  --url 'gemini.google.com' \
+  --name Gemini \
+  --icon google-gemini
+
+vb-webapp-install \
+  --category AI \
+  --url 'claude.ai/chats' \
+  --name Claude \
+  --icon claude
+
+vb-webapp-install \
+  --category AI \
+  --url 'chat.deepseek.com' \
+  --name DeepSeek \
+  --icon deepseek
+
+vb-webapp-install \
+  --category WebApp \
+  --url 'mail.google.com/mail/u/0/#inbox' \
+  --name Gmail \
+  --icon gmail
+
+vb-webapp-install \
+  --category WebApp \
+  --url 'youtube.com' \
+  --name YouTube \
+  --icon youtube
+
+vb-webapp-install \
+  --category WebApp \
+  --url 'reddit.com' \
+  --name Reddit \
+  --icon reddit
+
+vb-webapp-install \
+  --category WebApp \
+  --url 'github.com' \
+  --name GitHub \
+  --icon github
+
+vb-webapp-install \
+  --category WebApp \
+  --url 'x.com' \
+  --name X \
+  --icon twitter-x
---
+++ migrations/1776181616.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp "$VIBRANIUM/extras/icons/Vibranium/scalable/actions/vb-console.svg" ~/.local/share/icons/Vibranium/scalable/actions/vb-console.svg
+
+vb-theme-set-svg
