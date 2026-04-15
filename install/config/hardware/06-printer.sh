f2ed33de16cc93ca5cd435fab1b92be3dfcf77ba
shvedes <shved.personal@gmail.com>
2026-04-15 23:56:53 +0200

    chore: suppress tee output

--- install/config/hardware/06-printer.sh
+++ install/config/hardware/06-printer.sh
@@ -5,14 +5,14 @@
 # Disable multicast dns in resolved.
 # Avahi will provide this for better network printer discovery
 sudo mkdir -p /etc/systemd/resolved.conf.d
-echo -e "[Resolve]\nMulticastDNS=no" | sudo tee /etc/systemd/resolved.conf.d/10-disable-multicast.conf
+echo -e "[Resolve]\nMulticastDNS=no" | sudo tee /etc/systemd/resolved.conf.d/10-disable-multicast.conf >/dev/null
 
 # Enable mDNS resolution for .local domains
 sudo sed -i 's/^hosts:.*/hosts: mymachines mdns_minimal [NOTFOUND=return] resolve files myhostname dns/' /etc/nsswitch.conf
 
 # Enable automatically adding remote printers
 if ! grep -q '^CreateRemotePrinters Yes' /etc/cups/cups-browsed.conf; then
-  echo 'CreateRemotePrinters Yes' | sudo tee -a /etc/cups/cups-browsed.conf
+  echo 'CreateRemotePrinters Yes' | sudo tee -a /etc/cups/cups-browsed.conf >/dev/null
 fi
 
 sudo systemctl --quiet enable cups.service cups-browsed.service avahi-daemon.service
