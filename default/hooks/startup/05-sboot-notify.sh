c17965eb3fdb356a21e13f237e71caf99e1a8099
shvedes <shved.personal@gmail.com>
2026-04-15 23:05:19 +0200

    feat: notify the user if UEFI in Setup mode

--- default/hooks/startup/05-sboot-notify.sh
+++ default/hooks/startup/05-sboot-notify.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+SB_STATE="$(vb-cmd-sboot-status)"
+
 if [[ -f "$VIBRANIUM_STATE/secure-boot-setup.complete" ]]; then
   rm "$VIBRANIUM_STATE/secure-boot-setup.complete"
-
-  SB_STATE="$(vb-cmd-sboot-status)"
 
   if [[ "$SB_STATE" == "enabled" ]]; then
     notify-send -r 1 -t 10000 "Secure Boot Setup Wizard" "Secure Boot setup complete!"
@@ -15,5 +15,20 @@ if [[ -f "$VIBRANIUM_STATE/secure-boot-setup.complete" ]]; then
     msg="Secure Boot setup did not complete properly!\n"
     msg+="See 'sbctl status' for detailed info"
     notify-send -r 1 -t 60000 -u critical "Secure Boot Setup Wizard" "$msg"
+  fi
+else
+  if [[ "$SB_STATE" == "setup" ]]; then
+    STATE="$VIBRANIUM_STATE/sboot-notify.ignore"
+    if [[ ! -f "$STATE" ]]; then
+      msg="Secure Boot is disabled. Enable it in Vibranium Menu > Setup > Secure Boot\n\n"
+      msg+="Left-click to disable this message on startup\n"
+      msg+="Right-click to hide it until next login"
+      ACTION=$(notify-send --action=true -r 1 -t 60000 "UEFI Firmware In Setup Mode" "$msg")
+
+      if [[ -n "$ACTION" ]]; then
+        notify-send -r 1 -t 3000 "Vibranium" "Secure Boot notice has been disabled"
+        : >"$STATE"
+      fi
+    fi
   fi
 fi
