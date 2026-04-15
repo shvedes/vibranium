99df6f443630013aa02b889662ef04911e3267c0
shvedes <shved.personal@gmail.com>
2026-04-16 00:04:26 +0200

    feat: update desktop databse after modifying ~/.local/share/application

--- install/first-run/81-copy-desktop-files.sh
+++ install/first-run/81-copy-desktop-files.sh
@@ -3,3 +3,5 @@
 # Archive Manager
 cp /usr/share/applications/xarchiver.desktop ~/.local/share/applications
 sed -i '/Name=/s/Name=.*/Name=Archive Manager/' ~/.local/share/applications/xarchiver.desktop
+
+update-desktop-database ~/.local/share/applications
