225b4319151cca48433a5c0e02307974fa36dd0b
shvedes <shved.personal@gmail.com>
2026-04-15 22:15:21 +0200

    feat: bring back cups setup, now non-interactive

---
+++ install/config/hardware/06-printer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Copy-pasted from Omarchy.
+
+# Disable multicast dns in resolved.
+# Avahi will provide this for better network printer discovery
+sudo mkdir -p /etc/systemd/resolved.conf.d
+echo -e "[Resolve]\nMulticastDNS=no" | sudo tee /etc/systemd/resolved.conf.d/10-disable-multicast.conf
+
+# Enable mDNS resolution for .local domains
+sudo sed -i 's/^hosts:.*/hosts: mymachines mdns_minimal [NOTFOUND=return] resolve files myhostname dns/' /etc/nsswitch.conf
+
+# Enable automatically adding remote printers
+if ! grep -q '^CreateRemotePrinters Yes' /etc/cups/cups-browsed.conf; then
+  echo 'CreateRemotePrinters Yes' | sudo tee -a /etc/cups/cups-browsed.conf
+fi
+
+sudo systemctl --quiet enable cups.service cups-browsed.service avahi-daemon.service
