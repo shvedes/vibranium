153cc439be64534a4f48962beb98ada33a4a11e3
shvedes <shved.personal@gmail.com>
2026-04-16 10:48:51 +0200

    chore: silently install all necessay fs utilities

--- install/config/hardware/02-filesystem.sh
+++ install/config/hardware/02-filesystem.sh
@@ -8,23 +8,13 @@ if [[ "$ROOT_FS" == "btrfs" ]]; then
   _log_info "It's recommended to install extra tools for your filesystem."
   _log_info "These can be useful for maintenance and troubleshooting tasks."
 
-  if term::ask_yes_no Y "Would you like to install them?"; then
-    PACKAGES+=(btrfs-progs compsize)
-    UpdateSummary "System / Storage: installed BTRFS tools (btrfs-progs, compsize) - user choice"
-  fi
+  PACKAGES+=(btrfs-progs compsize)
+  UpdateSummary "System / Storage: installed BTRFS tools (btrfs-progs, compsize) - user choice"
 fi
 
 if [[ "$(lsblk -f)" =~ ntfs ]]; then
-  _log_info "NTFS partition detected"
-
-  if term::ask_yes_no Y "Do you want to install NTFS driver?"; then
-    _log_success "You will be able to access your NTFS partition without issues."
-    _log_success "Make sure Fast Boot is disabled in Windows settings,"
-    _log_success "otherwise the partition will be mounted as read-only."
-
-    PACKAGES+=(ntfs-3g)
-    UpdateSummary "System / Storage: installed NTFS-3G driver for NTFS partition access - user choice"
-  fi
+  PACKAGES+=(ntfs-3g)
+  UpdateSummary "System / Storage: installed NTFS-3G driver for NTFS partition access - user choice"
 fi
 
 if ((${#PACKAGES[@]} > 0)); then
