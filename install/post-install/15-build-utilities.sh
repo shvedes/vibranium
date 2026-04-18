c2001a5d9b756bb6fd17892eef58fbb8e59448e4
shvedes <shved.personal@gmail.com>
2026-04-18 20:23:52 +0200

    cleanup: move vb-cmd-edit-wm-config to https://github.com/shvedes/hyprland-config-editor

--- install/post-install/15-build-utilities.sh
+++ install/post-install/15-build-utilities.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
 _log_info "Building Vibranium utilities"
+_log_info "This might take a minute"
 
-sudo pacman -S --noconfirm rust &>/dev/null
+base_cwd="$(pwd)"
+delete_rust=false
 
-cd "$VIBRANIUM/contrib/vb-cmd-edit-wm-config"
-bash build.sh
+if ! command -v cargo >/dev/null; then
+  delete_rust=true
+  sudo pacman -S --noconfirm rust &>/dev/null
+fi
 
-sudo pacman -Rnsc --noconfirm rust &>/dev/null
+git clone -q https://github.com/shvedes/hyprland-config-editor /tmp/hce && cd /tmp/hce
 
-UpdateSummary "Compiled and installed vb-cmd-edit-wm-config"
+make install BINARY=vb-cmd-edit-wm-config PREFIX=~/.local/share/vibranium &>/dev/null
+
+rm -rf /tmp/hce
+
+if $delete_rust; then
+  sudo pacman -Rnsc --noconfirm rust &>/dev/null
+fi
+
+cd "$base_cwd"
