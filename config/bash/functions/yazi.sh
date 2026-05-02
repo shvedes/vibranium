54a60a0387a972d56427465f87b8c4f3865960b2
shvedes <shved.personal@gmail.com>
2026-05-02 17:30:59 +0200

    feat: add conditional yazi wrapper function

---
+++ config/bash/functions/yazi.sh
@@ -0,0 +1,14 @@
+if command -v yazi >/dev/null; then
+  function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+
+    command yazi "$@" --cwd-file="$tmp"
+    IFS= read -r -d '' cwd <"$tmp"
+
+    if [ "$cwd" != "$PWD" ] && [ -d "$cwd" ]; then
+      builtin cd -- "$cwd"
+    fi
+
+    command rm -f -- "$tmp"
+  }
+fi
