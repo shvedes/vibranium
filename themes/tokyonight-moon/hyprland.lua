b1a98153f3e6fbba88825be37c4e8381ddd2a350
shvedes <shved.personal@gmail.com>
2026-05-08 10:43:51 +0200

    cleanup: remove hyprland.lua; template system handles this pretty well

--- themes/tokyonight-moon/hyprland.lua
+++ themes/tokyonight-moon/hyprland.lua
@@ -3,8 +3,14 @@ ATTENTION = "rgb(f7768e)"
 hl.config({
   general = {
     col = {
-      active_border = { colors = { "rgb(7dcfff)", "rgb(9ece6a)" }, angle = 45 },
-      inactive_border = "rgb(353848)",
+      active_border = {
+        colors = {
+          "rgb(7dcfff)",
+          "rgb(9ece6a)"
+        },
+        angle = 45
+      },
+      inactive_border = "rgb(3e4565)",
     },
   },
 
@@ -14,4 +20,35 @@ hl.config({
     },
     background_color = "rgb(222436)",
   },
+
+  group = {
+    col = {
+      border_active = {
+        colors = {
+          "rgb(7dcfff)",
+          "rgb(9ece6a)"
+        },
+        angle = 45
+      },
+      border_inactive = "rgb(3e4565)",
+      border_locked_active = {
+        colors = {
+          "rgba(e0af68b3)",
+          "rgba(ff9e64b3)",
+          "rgba(f7768eb3)",
+        },
+        angle = 90
+      },
+      border_locked_inactive = "rgba(3e4565b3)",
+    },
+
+    groupbar = {
+      col = {
+        active = "rgb(7dcfff)",
+        inactive = "rgb(3e4565)",
+        locked_active = "rgb(7dcfff)",
+        locked_inactive = "rgb(3e4565)",
+      }
+    }
+  }
 })
--- themes/tokyonight-night/hyprland.lua
+++ themes/tokyonight-night/hyprland.lua
@@ -3,8 +3,14 @@ ATTENTION = "rgb(f7768e)"
 hl.config({
   general = {
     col = {
-      active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
-      inactive_border = "rgb(303146)",
+      active_border = {
+        colors = {
+          "rgb(7dcfff)",
+          "rgb(9ece6a)"
+        },
+        angle = 45
+      },
+      inactive_border = "rgb(424561)",
     },
   },
 
@@ -14,4 +20,35 @@ hl.config({
     },
     background_color = "rgb(1a1b26)",
   },
+
+  group = {
+    col = {
+      border_active = {
+        colors = {
+          "rgb(7dcfff)",
+          "rgb(9ece6a)"
+        },
+        angle = 45
+      },
+      border_inactive = "rgb(424561)",
+      border_locked_active = {
+        colors = {
+          "rgba(e0af68b3)",
+          "rgba(ff9e64b3)",
+          "rgba(f7768eb3)",
+        },
+        angle = 90
+      },
+      border_locked_inactive = "rgba(424561b3)",
+    },
+
+    groupbar = {
+      col = {
+        active = "rgb(7dcfff)",
+        inactive = "rgb(424561)",
+        locked_active = "rgb(7dcfff)",
+        locked_inactive = "rgb(424561)",
+      }
+    }
+  }
 })
