d75b5288e98a9362cdf13e0cc111011a52f9a75d
shvedes <shved.personal@gmail.com>
2026-05-09 16:41:11 +0200

    final lua migration

--- migrations/1778229831.sh
+++ migrations/1778229831.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cp "$VIBRANIUM"/config/hypr/hyprland.conf.d/event-* ~/.config/hypr/hyprland.conf.d/
+# no-op
 
-notify-send -t 30000 "Vibranium - New Keybindings Were Added" '
-To toggle a window group, press <b>SUPER + G</b>.
-<b>See Vibranium Menu > Help > Keybindings for more information</b>'
+exit 0
+
+# cp "$VIBRANIUM"/config/hypr/hyprland.conf.d/event-* ~/.config/hypr/hyprland.conf.d/
+#
+# notify-send -t 30000 "Vibranium - New Keybindings Were Added" '
+# To toggle a window group, press <b>SUPER + G</b>.
+# <b>See Vibranium Menu > Help > Keybindings for more information</b>'
