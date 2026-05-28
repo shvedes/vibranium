01ce6cd0cf45fb18808d3d36a9b588d67cf17162
shvedes <shved.personal@gmail.com>
2026-05-28 20:35:20 +0200

    feat: move startup/shutdown hooks in the /hooks folder

---
+++ config/vibranium/hooks/shutdown/example.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Here you can write your custom logic, which will
+# execute every time you log out from your current session.
+# Since this is inside the /shutdown subfolder, you may have
+# as many hooks as you want. They will run in alphanumerical order.
+#
+# Uncomment the line below and re-login to see the example in action.
+#
+# touch "$HOME/hello-from-${0##*/}"
---
+++ config/vibranium/hooks/startup/example.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Here you can write your custom logic, which will
+# execute every time you start a new session.
+# Since this is inside the /startup subfolder, you may have
+# as many hooks as you want. They will run in alphanumerical order.
+#
+# Uncomment the line below and restart your session
+# to see the example in action.
+#
+# notify-send -r 999 "Hello from ${0*##/}!"
