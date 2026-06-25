e6e0afd094a864bfae2faa735b9bc2913669c426
shvedes <shved.personal@gmail.com>
2026-06-26 00:00:39 +0200

    fix permissions

--- migrations/1782420326.sh
+++ migrations/1782420326.sh
@@ -5,15 +5,15 @@ RESET=$'\e[0m'
 SELF="${0##*/}"
 SELF="${SELF/.sh/}"
 
-comamnd sudo rm -f /etc/udev/rules.d/99-android.rules
-command cp "$VIBRANIUM/extras/etc/udev/rules.d/80-android.rules" /etc/udev/rules.d/
+sudo rm -f /etc/udev/rules.d/99-android.rules
+sudo cp "$VIBRANIUM/extras/etc/udev/rules.d/80-android.rules" /etc/udev/rules.d/
 
 if [[ "$CHASSIS_TYPE" != "vm" ]]; then
-  command sudo rm -f /etc/udev/rules.d/99-wifi-powersave.rules
+  sudo rm -f /etc/udev/rules.d/99-wifi-powersave.rules
 
   if vb-hw-battery; then
-    comamnd sudo rm -f /etc/udev/rules.d/99-vb-battery-alert.rules
-    command cp "$VIBRANIUM/extras/etc/udev/rules.d/10-battery-alert.rules" /etc/udev/rules.d/
+    sudo rm -f /etc/udev/rules.d/99-vb-battery-alert.rules
+    sudo cp "$VIBRANIUM/extras/etc/udev/rules.d/10-battery-alert.rules" /etc/udev/rules.d/
 
     sudo tee /etc/udev/rules.d/10-wifi-powersave.rules <<EOF2
 # This file was created by Vibranium install scripts.
@@ -31,8 +31,8 @@ ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="/usr/bin/su-bridge vb-cmd-
 EOF2
   fi
 
-  comamnd sudo rm -f /etc/udev/rules.d/99-vb-usb.rules
-  command cp "$VIBRANIUM/extras/etc/udev/rules.d/80-usb-pen.rules" /etc/udev/rules.d/
+  sudo rm -f /etc/udev/rules.d/99-vb-usb.rules
+  sudo cp "$VIBRANIUM/extras/etc/udev/rules.d/80-usb-pen.rules" /etc/udev/rules.d/
 fi
 
 sudo cp "$VIBRANIUM/extras/usr/local/bin/su-bridge" /usr/local/bin/
