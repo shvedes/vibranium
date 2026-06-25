fcd0473d3c651fe0127d882fa3907aa9416f9e26
shvedes <shved.personal@gmail.com>
2026-06-25 20:21:12 +0200

    fix: fix heredoc

--- install/config/hardware/221-framework16.sh
+++ install/config/hardware/221-framework16.sh
@@ -4,8 +4,8 @@
 
 if [[ $(</sys/class/dmi/id/sys_vendor) == "Framework" ]]; then
   if vb-hw-match "Laptop 16"; then
-    helpers::write_file /etc/udev/rules.d/50-framework16-qmk-hid.rules <<EOF
+    helpers::write_file /etc/udev/rules.d/50-framework16-qmk-hid.rules <<-EOF
     SUBSYSTEM=="hidraw", ATTRS{idVendor}=="32ac", ATTRS{idProduct}=="0012", MODE="0660", TAG+="uaccess"
-    EOF
+EOF
   fi
 fi
