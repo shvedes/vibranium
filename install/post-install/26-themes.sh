16dd9af366fa745132da3f8611cfe2f6c5c22045
shvedes <shved.personal@gmail.com>
2026-07-01 15:55:53 +0200

    feat: install additional themes conditionally

---
+++ install/post-install/26-themes.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+if ! term::ask_yes_no N "Would you like to install additional themes?"; then
+  exit 0
+fi
+
+URL="https://github.com/shvedes/vibranium-theme"
+DEST="$HOME/.config/vibranium/themes"
+THEMES=(
+  deep-forest
+  kihciahken
+  evergarden
+  ristretto
+  biscuit
+  dracula
+  kanso
+  aamis
+  demon
+  lumon
+  lush
+  lush-mono
+)
+
+mkdir -p "$DEST"
+
+for theme in "${THEMES[@]}"; do
+  helpers::log::info "Installing ${CYAN}$theme${RESET}"
+  git clone -q "${URL}-${theme}" "$DEST/$theme"
+done
+
