fcc609a80e120fe0fcc30ad2ff4f11d059165c00
shvedes <shved.personal@gmail.com>
2026-07-08 15:19:27 +0200

    chore: properly cleanup unused udev rules

--- install/post-install/99-cleanup.sh
+++ install/post-install/99-cleanup.sh
@@ -2,10 +2,9 @@
 
 # We have nothing to connect / listen to in a vm.
 if [[ "$CHASSIS_TYPE" == "vm" ]]; then
-  command sudo rm -rf /usr/local/bin/vb-android-notify
-  command sudo rm -rf /usr/local/bin/vb-battery-alert
-  command sudo rm -rf /usr/local/bin/vb-usb-notify
   command sudo rm -rf /etc/udev/rules.d/*
+elelif [[ "$CHASSIS_TYPE" == "desktop" ]]; then
+  command sudo rm -rf /etc/udev/rules.d/10-battery-alert.rules
 fi
 
 yay -Rnsc yay-debug --noconfirm &>/dev/null || true
