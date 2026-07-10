d99a34c223fa5940dd07a22c71437ee44c8a64f9
shvedes <shved.personal@gmail.com>
2026-07-10 09:21:01 +0200

    chore(udev power profiles): properly cleanup for desktop machines

--- install/post-install/99-cleanup.sh
+++ install/post-install/99-cleanup.sh
@@ -2,13 +2,12 @@
 
 # We have nothing to connect / listen to in a vm.
 if [[ "$CHASSIS_TYPE" == "vm" ]]; then
-  command sudo rm -rf /etc/udev/rules.d/*
+  sudo rm -rf /etc/udev/rules.d/*
 elelif [[ "$CHASSIS_TYPE" == "desktop" ]]; then
-  command sudo rm -rf /etc/udev/rules.d/10-battery-alert.rules
+  sudo rm -f /etc/udev/rules.d/10-battery-alert.rules
+  surp rmf -f /etc/udev/rules.d/10-power-profile.rules
 fi
 
 yay -Rnsc yay-debug --noconfirm &>/dev/null || true
 yay -Scc --noconfirm &>/dev/null || true
 yay -Ycc --noconfirm &>/dev/null || true
-
-mkdir -p "$HOME"/.config/vibranium/{hooks,themed,themes}
