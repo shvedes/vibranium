a9bfb0ad7e2a9b951b2aeeb32ae82e978222bad9
shvedes <shved.personal@gmail.com>
2026-07-22 01:35:10 +0200

    feat(vb-theme-set): pass $FOLDER_NAME to hooks

--- config/vibranium/hooks/theme/example.sh
+++ config/vibranium/hooks/theme/example.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 # Here you can write your own custom scripts
 # that will automatically execute every time you
 # change the active theme. Since this is inside the
 # /theme subfolder, you can have as many hooks as you want.
 # They will run in alphanumerical order.
 #
-# notify-send -r 999 "Hello from ${0##*/}!"
+# notify-send -r 999 "Hello from ${0##*/}!" "$THEME"
+#                                            ^
+#                                            The theme name is passed in this VAR, e.g. 'gruvbox-hard'
