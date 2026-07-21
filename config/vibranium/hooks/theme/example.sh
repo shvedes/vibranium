21c91f1f245203586c8a9c03a775d9daf68f7fe7
shvedes <shved.personal@gmail.com>
2026-07-22 01:46:53 +0200

    chore(theme/hook): update example

--- config/vibranium/hooks/theme/example.sh
+++ config/vibranium/hooks/theme/example.sh
@@ -6,6 +6,24 @@
 # /theme subfolder, you can have as many hooks as you want.
 # They will run in alphanumerical order.
 #
-# notify-send -r 999 "Hello from ${0##*/}!" "$THEME"
-#                                            ^
-#                                            The theme name is passed in this VAR, e.g. 'gruvbox-hard'
+# Here's some dummy yet still functional example:
+#
+# case "$THEME" in
+#       ^
+#       The theme name you just switched to.
+#       Automatically available to any script inside this folder.
+#   gruvbox-*-hard)
+#     TEMP=3000
+#     ;;
+#   gruvbox-*-medium)
+#     TEMP=3300
+#     ;;
+#   gruvbox-*)
+#     TEMP=3500
+#     ;;
+#   *)
+#     TEMP=4000
+#     ;;
+# esac
+#
+# vb-cmd-nightshift --set "$TEMP"
