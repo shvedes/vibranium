5fb45d215a5e761e2398b483344df0912ad772cb
shvedes <shved.personal@gmail.com>
2026-07-23 20:04:43 +0200

    fix(300-wifi-powersave.sh): fix typo in the path string

--- install/config/hardware/300-wifi-powersave.sh
+++ install/config/hardware/300-wifi-powersave.sh
@@ -11,14 +11,14 @@ if vb-hw-battery; then
 # This file was created by Vibranium install scripts.
 # #################################################### #
 # Laptop specific: toggle Wifi powersave based on charging state
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/su-bridge vb-cmd-wifi-powersave --on"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/su-bridge vb-cmd-wifi-powersave --off"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/local/bin/su-bridge vb-cmd-wifi-powersave --on"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/local/bin/su-bridge vb-cmd-wifi-powersave --off"
 EOF2
 else
   helpers::write_file /etc/udev/rules.d/10-wifi-powersave.rules << EOF2
 # This file was created by Vibranium install scripts.
 # #################################################### #
 # Desktop specific: disable Wifi's powersave mode on every boot
-ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="/usr/bin/su-bridge vb-cmd-wifi-powersave --off"
+ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="/usr/local/bin/su-bridge vb-cmd-wifi-powersave --off"
 EOF2
 fi
