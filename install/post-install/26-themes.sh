7939df285126b95cc0404575f0af4341aa0ca6a1
shvedes <shved.personal@gmail.com>
2026-07-23 13:24:39 +0200

    chore(install/themes): make a wrapper for theme installer

--- install/post-install/26-themes.sh
+++ install/post-install/26-themes.sh
@@ -4,31 +4,86 @@ if ! term::ask_yes_no N "Would you like to install additional themes?"; then
   exit 0
 fi
 
-URL="https://github.com/shvedes/vibranium-theme"
 DEST="$HOME/.config/vibranium/themes"
-THEMES=(
-  deep-forest
-  kihciahken
-  evergarden
-  ristretto
-  biscuit
-  dracula
-  kanso
-  antix
-  aamis
-  demon
-  lumon
-  lush
-  lush-mono
+PREFIX="vibranium-theme-"
+
+repos=$(curl -sf "https://api.github.com/users/shvedes/repos?per_page=100" \
+  | grep -oP '"name":\s*"\K[^"]+' \
+  | grep "^$PREFIX" \
+  | sort)
+
+if [[ -z $repos ]]; then
+  log Warn "No themes found"
+  exit 0
+fi
+
+mapfile -t themes <<< "$repos"
+total=${#themes[@]}
+current=0
+installed=0
+
+spinner_frames=(
+  '[=   ]'
+  '[ =  ]'
+  '[  = ]'
+  '[   =]'
 )
+
+frame_file="/tmp/${0##*/}.frame"
+tag_file="/tmp/${0##*/}.tag"
+
+echo 0 > "$frame_file"
+
+_spinner() {
+  local i
+  i=$(<"$2")
+
+  while true; do
+    local tag
+    tag=$(<"$4")
+    printf "\r\033[K%s Installing %s%s [%d/%d]" \
+      "${GRAY}${spinner_frames[$i]}${RESET}" \
+      "${CYAN}$1${RESET}" "$tag" "$3" "$5"
+
+    echo "$i" > "$2"
+    i=$(((i + 1) % ${#spinner_frames[@]}))
+    sleep 0.15
+  done
+}
+
+_stop_spinner() {
+  kill "$spinner_pid" 2> /dev/null
+  wait "$spinner_pid" 2> /dev/null
+}
 
 mkdir -p "$DEST"
 
-total=${#THEMES[@]}
-current=0
-
-for theme in "${THEMES[@]}"; do
+for theme in "${themes[@]}"; do
   ((current++))
-  git clone -q "${URL}-${theme}" "$DEST/$theme" &
-  term::spinner $! "${CYAN}[INFO]${RESET} Installing theme ${GRAY}[${current}/${total}]${RESET}"
+  name="${theme#$PREFIX}"
+  display="${name^}"
+  display="${display/-/ }"
+
+  echo "" > "$tag_file"
+  _spinner "$display" "$frame_file" "$current" "$tag_file" "$total" &
+  spinner_pid=$!
+
+  if git clone -q "https://github.com/shvedes/$theme" "$DEST/$name" 2> /dev/null; then
+    ((installed++))
+    echo " -> $DEST/$name" > "$tag_file"
+  else
+    echo " [FAIL]" > "$tag_file"
+  fi
+
+  _stop_spinner
 done
+
+rm -f "$frame_file" "$tag_file"
+
+if (( installed == 0 )); then
+  printf "\r\e[K%s[THEME]%s No themes installed\n" \
+    "$CYAN" "$RESET"
+else
+  printf "\r\e[K%s[THEME]%s %s%d%s themes installed\n" \
+    "$CYAN" "$RESET" "$GREEN" "$installed" "$RESET"
+fi
