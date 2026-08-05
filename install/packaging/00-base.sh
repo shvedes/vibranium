2e6f6a907486a0a58f875dec816673dbe376154f
shvedes <shved.personal@gmail.com>
2026-08-05 21:56:16 +0200

    fix(install): install hardware-detected packages (btrfs, nvme, ntfs, sata)

--- install/packaging/00-base.sh
+++ install/packaging/00-base.sh
@@ -3,6 +3,10 @@
 mapfile -t packages < <(grep -Ev '^(#|$)' "$VIBRANIUM/install/vb-base.pkgs")
 mapfile -t fonts < <(grep -Ev '^(#|$)' "$VIBRANIUM/install/vb-fonts.pkgs")
 
+# Keep hardware-detected packages (btrfs, nvme, ntfs, sata)
+# added by the hardware phase before this one
+mapfile -t hardware < /tmp/vibranium.packages 2> /dev/null || true
+
 packages+=(${fonts[@]})
 
 if [[ "$CHASSIS_TYPE" != vm ]]; then
@@ -14,6 +18,6 @@ fi
 
 rm -f /tmp/vibranium.packages
 
-for pkg in "${packages[@]}"; do
+for pkg in "${packages[@]}" "${hardware[@]}"; do
   printf "%s\n" "$pkg" >>/tmp/vibranium.packages
 done
