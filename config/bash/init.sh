1709c1b25da983673d8e6f352cea67de1f2e7696
shvedes <shved.personal@gmail.com>
2026-08-06 14:30:23 +0200

    fix(shell/bash): create history file if doesn't exist

--- config/bash/init.sh
+++ config/bash/init.sh
@@ -1,10 +1,11 @@
+# shellcheck disable=all
+
 # If not running interactively, don't do anything
 if [[ $- != *i* ]]; then
   return
 fi
 
-# Don't throw error
-# when globs don't expand.
+# Don't throw error when globs don't expand.
 shopt -s nullglob
 
 YELLOW=$'\e[0;33m'
@@ -12,12 +13,18 @@ GREEN=$'\e[0;32m'
 RED=$'\e[0;31m'
 RESET=$'\e[0m'
 
+# Use :- in case if launched in TTY
 BASH_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/bash"
 
-for f in "$BASH_CONFIG_DIR"/{functions,aliases}/*.sh; do
-  source "$f"
-done
+# Create history folder / file if doesn't exist yet.
+# bash does not create it automatically, so cover it.
+if ! [[ -d "${XDG_DATA_HOME:-$HOME/.local/state}/bash" ]]; then
+  mkdir -p "${XDG_DATA_HOME:-$HOME/.local/state}/bash"
+  : > "${XDG_DATA_HOME:-$HOME/.local/state}/bash/history"
+fi
 
+source "$BASH_CONFIG_DIR/functions.sh"
 source "$BASH_CONFIG_DIR/greeting.sh"
+source "$BASH_CONFIG_DIR/aliases.sh"
 source "$BASH_CONFIG_DIR/prompt.sh"
 source "$BASH_CONFIG_DIR/binds.sh"
