a4fb04c10183e0a2b95b4b49b6232b14ccae9cd6
shvedes <shved.personal@gmail.com>
2026-08-07 23:26:30 +0200

    move: vb-util-startup -> vb-core-startup

--- default/hypr/autostart.lua
+++ default/hypr/autostart.lua
@@ -1,7 +1,7 @@
 hl.on("hyprland.start", function()
   hl.exec_cmd("vb-cursor-set --quiet")
   hl.exec_cmd("vb-refresh-dunst")
-  hl.exec_cmd("vb-util-startup")
+  hl.exec_cmd("vb-core-startup")
 
   hl.exec_cmd("systemctl --user import-environment $(env | cut -d'=' -f 1)")
   hl.exec_cmd("dbus-update-activation-environment --systemd --all")
